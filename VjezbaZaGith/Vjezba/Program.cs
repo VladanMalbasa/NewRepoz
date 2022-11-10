6bb44685359b29be3ea432d4a041d83a5d0de2f7
DESKTOP-BF1MGND\Vladan Malbasa <v.malbasa96@gmail.com>
2022-11-11 00:39:20 +0100

    Finish 2 zadatka

--- VjezbaZaGith/Vjezba/Program.cs
+++ VjezbaZaGith/Vjezba/Program.cs
@@ -1,22 +1,52 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+
 
 namespace Vjezba
 {
     internal class Program
     {
+        // Pisanje reference za browsera
+        IWebDriver driver = new ChromeDriver();
+
         static void Main(string[] args)
         {
+       
+        }
+        [SetUp]
+        public void Initilaze() 
+        {
+            // Putanja za Google
+            driver.Navigate().GoToUrl("http://www.google.com");
+            Console.WriteLine("Otvorio se URL!");
+        }
+        [Test]
+        public void ExecuteTest()
+        {
+            IWebElement btnAgreeCookie = driver.FindElement(By.XPath("//*[@id='L2AGLb']/div"));
+            btnAgreeCookie.Click();
 
+            IWebElement searhBox = driver.FindElement(By.Name("q"));
+            searhBox.SendKeys("Audi A4");
 
+            Thread.Sleep(2000);
 
+            IWebElement searchButton = driver.FindElement(By.XPath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[1]"));
+            searchButton.Click();              
 
-
-
-
+        }
+        [TearDown]
+        public void CleanUp() 
+        {
+            driver.Close();
+            Console.WriteLine("Browser se zatvorio!");
         }
     }
 }
---
+++ VjezbaZaGith/Vjezna1/MetodeZaVjezbu.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+
+namespace Vjezba1
+{
+    public class MetodeZaVjezbu
+    {
+        //EnterText
+        public static void EnterText(IWebDriver driver, string element, string value, string elementtype) 
+        {
+            if (elementtype == "Id")
+            {
+                driver.FindElement(By.Id(element)).SendKeys(value);
+            }
+            if (elementtype == "Name")
+            {
+                driver.FindElement(By.Name(element)).SendKeys(value);
+            }                 
+        }
+        //Click za dugme, Chechbox.....
+        public static void Click(IWebDriver driver, string element, string elementtype)
+        {
+            if (elementtype == "Id")
+            {
+                driver.FindElement(By.Id(element)).Click();
+            }
+            if (elementtype == "Name")
+            {
+                driver.FindElement(By.Name(element)).Click();
+            }
+        }
+        // Odabir Dropdown kontrole
+        public static void SelectDropdown(IWebDriver driver, string element, string value, string elementtype) 
+        {            
+            if (elementtype == "Id")
+            {
+                new SelectElement (driver.FindElement(By.Id(element))).SelectByText(value);
+            }
+            if (elementtype == "Name")
+            {
+                new SelectElement(driver.FindElement(By.Name(element))).SelectByText(value);
+            }
+        }
+    }
+}
